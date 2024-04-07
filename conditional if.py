e4bb1634f6805d3039722ab536f617d64b576838
menza-spec <khamis.harrison@gmail.com>
2024-04-07 11:12:59 +0300

    Add files via upload

---
+++ conditional if.py
@@ -0,0 +1,5 @@
+age=int(input("Please enter your age:"))
+if age >18:
+    print("You are allowed to vote")
+    #input please enter your age will take a string value
+    #age =int, will convert the string value into a interger
